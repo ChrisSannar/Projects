22482f42468645f4a5361551a2e2a20d53a89e0d
Christopher Sannar <chris.sannar.dev@gmail.com>
2021-01-23 11:21:17 -0700

    Basic Graph creation from text input
    Wrote a test and got the graph to insert new nodes and edges based on a properly formatted text input.

--- golang/algo_and_data_structures/datastructures/graph.go
+++ golang/algo_and_data_structures/datastructures/graph.go
@@ -1,6 +1,10 @@
 package datastructures
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 type Edge struct {
 	weight int
@@ -9,23 +13,63 @@ type Edge struct {
 }
 
 type Node struct {
-	edges  []Edge
+	edges  []*Edge
 	value  string
 	weight int
 }
 
 type Graph struct {
 	size  int
-	nodes map[string]Node
+	nodes map[string]*Node
 }
 
-func main() {
-	fmt.Println("Working")
+func (graph *Graph) print() {
+	for _, node := range graph.nodes {
+		fmt.Println(node)
+	}
+}
+
+func appendEdge(node Node, edge *Edge) []*Edge {
+	edges := node.edges
+	edges = append(edges, edge)
+	return edges
 }
 
 func CreateWeightedGraphFromInput(nodes []string, edges []string) Graph {
+	nodeMap := make(map[string]*Node)
+	size := 0
+
+	// Set all the nodes in the graph with their default weights of -1
+	for _, nodeStr := range nodes {
+		nodeMap[nodeStr] = &Node{
+			edges:  []*Edge{},
+			value:  nodeStr,
+			weight: -1,
+		}
+		size++
+	}
+
+	// Include all the edges
+	regex := regexp.MustCompile("\\s*(\\(|\\))\\s*") // Proper regex for parsing the input
+	for _, edgeStr := range edges {
+
+		// Split the values out and parse them into an edge
+		split := regex.Split(edgeStr, -1)
+		weight, _ := strconv.Atoi(split[1])
+		edge := &Edge{
+			weight: weight,
+			from:   *nodeMap[split[0]],
+			to:     *nodeMap[split[2]],
+		}
+
+		// Then insert that edge into the given Node
+		noders := nodeMap[split[0]]
+		noders.edges = append(noders.edges, edge)
+	}
+
+	// Resulting graph object
 	return Graph{
-		size:  0,
-		nodes: make(map[string]Node),
+		size:  size,
+		nodes: nodeMap,
 	}
 }
