67c2a6239b25954ddc4ead527e33e0cff2b4b117
Christopher Sannar <chris.sannar.dev@gmail.com>
2021-02-08 10:44:41 -0700

    Resizing setup
    Resizing is proving to be rather difficult. I decided to first capture getting the cursor to change on the proper edge and therefore set it up to resize in that direction. Also set up the function to do the actual resizing, but it's just a start. I'll probably employ the same technique I did with the drag (move the mouse to it if we're 'resizing')

--- javascript/react/graphics/src/App.js
+++ javascript/react/graphics/src/App.js
@@ -8,7 +8,9 @@ function App() {
   const [mousePosition, setMousePosition] = useState([0, 0])
 
   return (
-    <div className="App" onMouseMove={(e) => setMousePosition([e.clientX, e.clientY])}>
+    <div 
+    className="App"
+    onMouseMove={(e) => setMousePosition([e.clientX, e.clientY])}>
       <MouseContext.Provider value={mousePosition}>
         <Box width="200" height="150" />
         <Box width="400" height="300" />
--- javascript/react/graphics/src/components/Box.js
+++ javascript/react/graphics/src/components/Box.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useRef, useContext } from 'react'
+import React, { useState, useEffect, useLayoutEffect, useRef, useContext } from 'react'
 import { MouseContext } from '../App.js'
 import "./Box.css"
 
-function Box(props) {
+// How many pixels away from the border before we consider it for resizing 
+const BORDER_MARGIN = 10
 
+function Box(props) {
   const { width, height } = props
 
   // Use Ref to keep a shallow render
@@ -15,13 +17,31 @@ function Box(props) {
   // Keeping track of the mouse position and state
   const [mouseBoxPosition, setMouseBoxPosition] = useState([0, 0])
   const [dragging, setDragging] = useState(false)
-
-  const [boxStyling] = useState({
+  const [resizing, setResizing] = useState(false)
+  const [edge, setEdge] = useState("")
+  const [boxStyling, setBoxStyling] = useState({
     width: (width ?? "0") + "px",
     height: (height ?? "") + "px",
+    cursor: "auto"
   })
 
-  // Moves the current box to this position
+  // When we click the mouse down, we need to set the position and set dragging to true
+  const handleHeaderMouseDown = (event) => {
+    const rect = box.current.getBoundingClientRect()
+    setMouseBoxPosition([event.clientX - rect.x, event.clientY - rect.y])
+    setDragging(true)
+  }
+
+  // Make sure when we go out but are still dragging, we move the mouse with it
+  const handleMouseOut = () => {
+    if (dragging) {
+      moveBoxToMouse(mouseX, mouseY)
+    } else {
+      setDragging(false)
+    }
+  }
+
+  // Moves the current box to this position (relative to where the mouse is)
   const moveBoxToMouse = (x, y) => {
     const [mouseBoxX, mouseBoxY] = mouseBoxPosition;
     const left = x - mouseBoxX
@@ -30,34 +50,117 @@ function Box(props) {
     box.current.style.top = top + "px"
   }
 
-  // Just make sure whenever we re-render the application that we move the box the the necessary position
-  useEffect(() => {
+  // Checks if the current x/y position is near the box (within the BORDER_MARGIN)
+  const nearBox = (x, y) => {
+    const rect = box.current.getBoundingClientRect()
+    return x > (rect.left - BORDER_MARGIN) &&
+    x < (rect.right + BORDER_MARGIN) &&
+    y > (rect.top - BORDER_MARGIN) &&
+    y < (rect.bottom + BORDER_MARGIN)
+  }
+
+  // Determines if the edge is close enough to consider "editing"
+  const getEdgeWithinMargin = (x, y) => {
+    let result = ""
+    if (nearBox(x, y)) {
+      const rect = box.current.getBoundingClientRect()
+      // if (Math.abs(y - rect.top) < BORDER_MARGIN) {
+      //   result += "top"
+      // }
+      if (Math.abs(y - rect.bottom) < BORDER_MARGIN) {
+        result += "bottom"
+      }
+      if (Math.abs(x - rect.left) < BORDER_MARGIN) {
+        result += "left"
+      }
+      if (Math.abs(x - rect.right) < BORDER_MARGIN) {
+        result += "right"
+      }
+    }
+    return result;
+  }
+
+  // Sets the cursor and the current resize configuration
+  const setCursorToEdge = (edge) => {
+    switch (edge) {
+      case "top":
+      case "bottom":
+        setBoxStyling({ ...boxStyling, cursor: "ns-resize"})
+        break;
+      case "left":
+      case "right":
+        setBoxStyling({ ...boxStyling, cursor: "ew-resize"})
+        break;
+      case "bottomleft":
+      case "topright":
+        setBoxStyling({ ...boxStyling, cursor: "nesw-resize"})
+        break;
+      case "topleft":
+      case "bottomright":
+        setBoxStyling({ ...boxStyling, cursor: "nwse-resize"})
+        break;
+      default:
+        setBoxStyling({ ...boxStyling, cursor: "auto"})
+        break;
+    }
+  }
+
+  // const [mouseBoxX, mouseBoxY] = mouseBoxPosition;
+  //   const left = x - mouseBoxX
+  //   const top = y - mouseBoxY
+  //   box.current.style.left = left + "px"
+  //   box.current.style.top = top + "px"
+
+  // resizes the box based on were the mouse is
+  const resizeBoxToMouse = (edge, x, y) => {
+    const { 
+      width, 
+      height,
+      top,
+      bottom,
+      left,
+      right 
+    } = box.current.getBoundingClientRect()
+    console.log(edge, height, bottom, y)
+
+  }
+
+  // Just make sure whenever we re-render the application that we move the box to the necessary position
+  useLayoutEffect(() => {
+
+    // Move the box if we're dragging
     if (dragging) {
       moveBoxToMouse(mouseX, mouseY)
+    } else {
+      const edge = getEdgeWithinMargin(mouseX, mouseY)
+      setEdge(edge)
+      if (edge) {
+        setCursorToEdge(edge)
+      } else {
+        setBoxStyling({ ...boxStyling, cursor: "auto"})
+      }
     }
-  })
+
+    if (resizing && edge) {
+      resizeBoxToMouse(edge, mouseX, mouseY)
+    }
+  }, [mouseX, mouseY])
 
   return (
     <div 
       className="Box"
       ref={box}
-      style={boxStyling}>
+      style={boxStyling}
+      // onMouseMove={handleMouseMove}
+      onMouseDown={() => setResizing(!dragging)}
+      onMouseUp={() => setResizing(false)}
+      >
         <div 
         className="headerBar"
-        onMouseDown={(event) => {
-          const rect = box.current.getBoundingClientRect()
-          setMouseBoxPosition([event.clientX - rect.x, event.clientY - rect.y])
-          setDragging(true)
-        }} 
+        style={{ cursor: dragging ? "grabbing" : "grab"}}
+        onMouseDown={handleHeaderMouseDown} 
         onMouseUp={() => setDragging(false)}
-        onMouseOut={() => {
-          console.log("OUT", dragging)
-          if (dragging) {
-            moveBoxToMouse(mouseX, mouseY)
-          } else {
-            setDragging(false)
-          }
-        }}></div>
+        onMouseOut={handleMouseOut}></div>
     </div>
   )
 }
