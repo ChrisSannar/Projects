285101c301901dec11e91a0782d2169e1abfcdaa
Christopher Sannar <chris.sannar.dev@gmail.com>
2021-02-07 17:15:28 -0700

    Got the drag to work, but have some issues
    So I've got the box to move when you click and drag it by using a few state variables (a 'draging' when we're clicked down and keeping track of the mouse movements in the state). However, there is an issue with moving the mouse too fast: the event registeres when the mouse is outside the box and still thinks it's 'dragging' also need to specify where on the box that can be clicked

--- javascript/react/graphics/src/components/Box.js
+++ javascript/react/graphics/src/components/Box.js
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from 'react'
 
 function Box() {
+  
+  // Default Box size
   const [boxWidth, setBoxWidth] = useState(100)
   const [boxHeight, setBoxHeight] = useState(100)
   
+  // Default box position
   const [boxLeft, setBoxLeft] = useState(0)
   const [boxTop, setBoxTop] = useState(0)
 
-  // const [dragging, setDragging] = useState(false)
+  const [mouseX, setMouseX] = useState(0)
+  const [mouseY, setMouseY] = useState(0)
+
+  const [dragging, setDragging] = useState(false)
+
+  // Box styling
   const [boxStyling, setBoxStyling] = useState({
     backgroundColor: "red",
+    position: "absolute",
+    cursor: "pointer",
     width: `${boxWidth}px`,
     height: `${boxHeight}px`,
-    position: "absolute",
-    left: `10px`,
-    top: `10px`,
-    // left: `${boxLeft}px`,
-    // top: `${boxTop}px`,
+    left: `${boxLeft}px`,
+    top: `${boxTop}px`,
   })
 
+  // When we change any of the box stylings, make sure to update them
   useEffect(() => {
     setBoxStyling({
       ...boxStyling,
@@ -27,21 +35,43 @@ function Box() {
       left: `${boxLeft}px`,
       top: `${boxTop}px`,
     })
+    // eslint-disable-next-line
   }, [boxWidth, boxHeight, boxLeft, boxTop])
 
-  const boxClick = (event) => {
-    changeBoxSize(10, 10)
-    setBoxLeft(boxLeft + 10)
-    setBoxTop(boxTop + 10)
-  }
-
+  // Change the boxes size by the amount of the delta input
   const changeBoxSize = (deltaWidth, deltaHeight) => {
     setBoxWidth(boxWidth + deltaWidth)
     setBoxHeight(boxHeight + deltaHeight)
   }
 
+  // Changes the boxes position by the amount of the delta input
+  const changeBoxPosition = (deltaLeft, deltaTop) => {
+    setBoxLeft(boxLeft + deltaLeft)
+    setBoxTop(boxTop + deltaTop)
+  }
+
+  // When
+  const moveMouse = (event) => {
+
+    // Set the mouse information including the change
+    const xDelta = event.clientX - mouseX;
+    const yDelta = event.clientY - mouseY;
+    setMouseX(event.clientX)
+    setMouseY(event.clientY)
+
+    // If we're in a "dragging" moment then move the box position 
+    if (dragging) {
+      changeBoxPosition(xDelta, yDelta)
+    }
+  }
+
   return (
-    <div className="Box" style={boxStyling} onClick={boxClick}>
+    <div 
+      className="Box" 
+      style={boxStyling} 
+      onMouseMove={moveMouse}
+      onMouseDown={() =>  setDragging(true)} 
+      onMouseUp={() => setDragging(false)}>
       Imma box
     </div>
   )
